f9251e1a5a4624970302732064c78e4562ef2481
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-06-28 16:33:19 -0400

    add random generator function

--- js/app.js
+++ js/app.js
@@ -68,3 +68,8 @@ document.addEventListener('keyup', function(e) {
 
     // player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//returns a random integer between min-max inclusive
+function randomInt(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}

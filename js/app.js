1174397b411cf65bac35ab5d4b3b3d0201b2c008
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-06-29 18:18:03 -0400

    create class for every moving object

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+class MovingObjects{
+  constructor(name){
+    this.sprite='images/'+name+'.png';
     this.speed=randomInt(70,300);
     this.x=-150;
-    let positionX= -150;
     let columnPlacement= randomInt(1,3);
     switch (columnPlacement) {
       case 1:
@@ -20,21 +15,30 @@ var Enemy = function() {
       default:
         this.y=70*3+20;
     }
-};
+  }
+
+  update(dt){
+    this.x+=dt*this.speed;
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+  }
+
+  render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  this.x+=dt*this.speed;
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 
 class Players{
@@ -80,7 +84,7 @@ class Players{
 let allEnemies=[];
 
 const intervalID= window.setInterval(function(){
-  let enemyInstances= new Enemy();
+  let enemyInstances= new MovingObjects('enemy-bug');
   allEnemies.push(enemyInstances);
 },1000);
 
